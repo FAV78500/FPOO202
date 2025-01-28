0ad85da1b4323e00aa24fb7c7f42e1e2e7a25911
DeadFox785 <thafox785@gmail.com>
2025-01-28 14:41:49 -0600

    Cree codigo para pago de horas

---
+++ HORAS/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+double hora ;
+double coste;
+        System.out.println("Escriba las horas trabajadas");
+hora = scanner.nextDouble();
+System.out.println("Ingrese el valor de las horas trabajadas");
+coste = scanner.nextInt();
+System.out.println("El total del pago es :");
+System.out.println(hora *= coste);
+    }
+}
