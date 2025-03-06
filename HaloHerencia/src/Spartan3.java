69e9a82eb1535a4d2b271ac34dfa1d75363d6b4c
FAV78500 <123046044@upq.edu.mx>
2025-03-06 15:02:19 -0600

    Modifique y termine de hacer la practica 14

--- HaloHerencia/src/Spartan3.java
+++
@@ -1,8 +0,0 @@
-public class Spartan3 extends Spartan{
-public Spartan3(String nombre, int salud, String arma, int escudo){
-    super(nombre,salud,arma,escudo=80);
-}
-public void camuflajeActivo(){
-    System.out.println(nombre+(" activo camuflaje para volverse invisible"));
-}
-}
